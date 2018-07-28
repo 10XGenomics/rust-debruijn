0942586e45f640c06ac321ae16f1d39a820c079b
Travis CI User <travis@example.org>
2018-07-28 00:38:16 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -10,6 +10,7 @@ ALIASES['debruijn'] = {};
 ALIASES['debruijn'] = {};
 ALIASES['debruijn'] = {};
 ALIASES['debruijn'] = {};
+ALIASES['debruijn'] = {};
 ALIASES['dtoa'] = {};
 ALIASES['dtoa'] = {};
 ALIASES['either'] = {};
