3c1d77174768b91a3052d354d02fdc6c8d4ff6f9
Travis CI User <travis@example.org>
2019-04-14 18:16:46 +0000

    Automatic Travis documentation build

---
+++ master/implementors/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["debruijn"] = [{text:"impl&lt;'a, K:&nbsp;<a class=\"trait\" href=\"debruijn/trait.Kmer.html\" title=\"trait debruijn::Kmer\">Kmer</a> + 'a, D:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + 'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"debruijn/graph/struct.NodeKmerIter.html\" title=\"struct debruijn::graph::NodeKmerIter\">NodeKmerIter</a>&lt;'a, K, D&gt;",synthetic:false,types:["debruijn::graph::NodeKmerIter"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- master/source-files.js
+++ master/source-files.js
@@ -5,3 +5,4 @@ sourcesIndex['debruijn'] = {"name":"","dirs":[],"files":["clean_graph.rs","compr
 sourcesIndex['debruijn'] = {"name":"","dirs":[],"files":["clean_graph.rs","compression.rs","dna_string.rs","filter.rs","graph.rs","kmer.rs","lib.rs","msp.rs","test.rs","vmer.rs"]};
 sourcesIndex['debruijn'] = {"name":"","dirs":[],"files":["clean_graph.rs","compression.rs","dna_string.rs","filter.rs","graph.rs","kmer.rs","lib.rs","msp.rs","test.rs","vmer.rs"]};
 sourcesIndex['debruijn'] = {"name":"","dirs":[],"files":["clean_graph.rs","compression.rs","dna_string.rs","filter.rs","graph.rs","kmer.rs","lib.rs","msp.rs","test.rs","vmer.rs"]};
+createSourceSidebar();
